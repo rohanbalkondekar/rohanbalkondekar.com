f0ffc80e810b62e849c9dd067f2ef2d9b8f37256
Rohan Balkondekar <brohan5501@gmail.com>
2023-10-11 03:28:13 +0530

    3JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import GradientCircle from './components/GradientCircle'
+import NBodySimulation from './components/NBodySimulation'
 
 function App() {
   return (
     <>
       <GradientCircle />
       <Header />
+      <NBodySimulation />
     </>
   )
 }
