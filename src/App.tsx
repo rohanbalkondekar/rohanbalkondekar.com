3e255746ee4fb6df5055011409de85525d8ed300
Rohan Balkondekar <brohan5501@gmail.com>
2023-10-11 02:29:12 +0530

    rohanbalkondekar.com

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import GradientCircle from './components/GradientCircle'
+
+function App() {
+  return (
+    <>
+      <GradientCircle />
+      <Header />
+    </>
+  )
+}
+
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <header className="header">
+        <p className="header-title">Rohan Balkondekar</p>
+        <p className="header-subtitle">Software Engineer</p>
+        <p className="header-description">I Build Scalable, Performant and Cost Effective AI Products for Enterprise</p>
+      </header>
+    )
+}
+
+export default Header
