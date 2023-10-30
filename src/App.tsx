4dbb2e87d76b2e2286171d4f709bef7adf08e8c1
Rohan Balkondekar <brohan5501@gmail.com>
2023-10-30 13:31:29 +0530

    Added Spline 3D element

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Header from './components/Header'
 import GradientCircle from './components/GradientCircle'
-import NBodySimulation from './components/NBodySimulation'
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
     <>
+      <div className="spline-container">
+      <Spline scene="https://prod.spline.design/7xbjlBkVVxdgT4gV/scene.splinecode" />
+      </div>
       <GradientCircle />
       <Header />
-      <NBodySimulation />
+      {/* <Spline scene="https://prod.spline.design/7xbjlBkVVxdgT4gV/scene.splinecode" /> */}
     </>
   )
 }
