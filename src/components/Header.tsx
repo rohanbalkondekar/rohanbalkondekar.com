0a93cb0e12c073b1a337e5208dd39ba0360aabed
Rohan-Balkondekar-Lyzr <rohan@lyzr.ai>
2024-01-22 01:29:16 +0530

    added cool peacock

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import './Header.css'
 
-function Header(){
-    return(
-        <header className="header">
-        <p className="header-title">Rohan Balkondekar</p>
+function Header() {
+  return (
+    <header className="header">
+      <p className="header-title">Rohan Balkondekar</p>
+      <div className="header-subtitle-container">
         <p className="header-subtitle">Software Engineer</p>
-        <p className="header-description">I Build Scalable, Performant and Cost Effective AI Products for Enterprise</p>
-      </header>
-    )
+        <img src="../../public/Peacock.png" alt="Peacock" width="25" height="25" />
+      </div>
+      <p className="header-description">I Build Scalable, Performant and Cost Effective AI Products for Enterprise</p>
+    </header>
+  )
 }
 
 export default Header
